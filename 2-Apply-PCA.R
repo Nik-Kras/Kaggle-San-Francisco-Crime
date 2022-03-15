e0ba234672e5229b4e6cb8879115fb7f1ef80671
Nikita <nikita.kras.kyiv@gmail.com>
2022-03-15 21:23:46 +0000

    Made a table of plots - feature depends on another feature. It could be seen that X and Y fields have narrow variance in the plots so the PCA can be implemented. The plots are saved in data\output folder

--- 2-Apply-PCA.R
+++ 2-Apply-PCA.R
@@ -13,3 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Feature analysis --------------------------------------------
+print("Making dependencies plot...")
+cat(sprintf("Time: %s\n", Sys.time()))
+
+# Take a smaller dataset to make a plot of it to see if the PCA can be applied
+step = 3000
+test_step <- test[seq(1, nrow(test), step),]
+plot(test_step)
+rm(test_20, test_step, step)
+
+cat(sprintf("Time: %s\n", Sys.time()))
+print("Making dependencies plot is finished")
+cat('The plot shows that the variance of X and partically Y is lower than others.
+     It means that PCA analysis may be used to reduce the number of features, 
+     Replacing them by new orthogonal ones.')
+# PCA dimension reduction  ------------------------------------
+print("PCA analysis...")
+cat(sprintf("Time: %s\n", Sys.time()))
+
+pc <- prcomp(train)
+
+cat(sprintf("Time: %s\n", Sys.time()))
+print("PCA analysis is finished")
