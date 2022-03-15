50c4571e2ce9847b26c30d56572f5b81e01ff545
Nikita <nikita.kras.kyiv@gmail.com>
2022-03-15 21:20:43 +0000

    Finished all converts from strings to numbers. Separated Labels from Training set. Made a bar plot of Categories distributions and saved labels in numerical format

--- 0-Load-and-convert.R
+++ 0-Load-and-convert.R
@@ -19,6 +19,8 @@ library(scales)
 library(class)
 library(data.table)
 
+set.seed(1203)
+
 rm(list=ls())
 
 # Load data ---------------------------------------------------
@@ -37,6 +39,7 @@ print(head(train))
 # They are not present in testing data
 # Reorder for comfort
 print("Deleting features and reordering...")
+cat(sprintf("Time: %s\n", Sys.time()))
 
 train[, c("Descript","Resolution","Address"):=NULL]
 setcolorder(train, c("Dates", 
@@ -57,6 +60,7 @@ print(head(train))
 
 # Expand dates features --------------------------------------
 print("Increasing feature count by dates detalisation...")
+cat(sprintf("Time: %s\n", Sys.time()))
 ExpandDates <- function(Dates){
   
   dates    <- Dates
@@ -75,9 +79,11 @@ ExpandDates <- function(Dates){
 
 expanded_time <- ExpandDates(train$Dates)
 train <- cbind(expanded_time, train)  
+train[, c("Dates"):=NULL]
 
 expanded_time <- ExpandDates(test$Dates)
 test <- cbind(expanded_time, test)
+test[, c("Dates"):=NULL]
 
 rm(expanded_time)
 
@@ -85,3 +91,79 @@ cat(sprintf("Time: %s\n", Sys.time()))
 print("Dates detalisation is finished")
 print("The training data currently looks like: ")
 print(head(train))
+
+# Convert strings to numbers ---------------------------------
+print("Converting strings to numbers...")
+cat(sprintf("Time: %s\n", Sys.time()))
+
+key_names <- unique(train$DayOfWeek)
+for (i in 1:length(key_names))
+{
+  train$DayOfWeek[train$DayOfWeek == key_names[i]]  <- i
+  test$DayOfWeek [test$DayOfWeek  == key_names[i]]  <- i
+}
+
+key_names <- unique(train$PdDistrict)
+for (i in 1:length(key_names))
+{
+  train$PdDistrict[train$PdDistrict == key_names[i]]  <- i
+  test$PdDistrict [test$PdDistrict  == key_names[i]]  <- i
+}
+
+rm(i, key_names)
+
+cat(sprintf("Time: %s\n", Sys.time()))
+print("Converting strings to numbers has been finished.")
+print(head(train))
+
+# Separate labels --------------------------------------------
+print("Separating labels...")
+cat(sprintf("Time: %s\n", Sys.time()))
+
+# Separate thr Category from the train data
+Labels_train <- train$Category
+train[, c("Category"):=NULL]
+
+print("Currently labels looks like this")
+print(Labels_train[1:10])
+
+SubmitTable <- data.table(read.csv("./data/dataset/sampleSubmission.csv",
+                                   check.names=FALSE))
+# First name is "ID" all next are categories
+ListCategories <- colnames(SubmitTable)[-1]   
+                          
+# Convert labels to numbers
+for (i in 1:length(ListCategories))
+{
+  Labels_train[Labels_train == ListCategories[i]] <- as.integer(i)
+}
+
+Labels_train_numbers <- as.integer(Labels_train)
+
+print("UPD: Currently labels looks like this")
+print(Labels_train_numbers[1:10])
+
+cat(sprintf("Time: %s\n", Sys.time()))
+print("Separating labels is finished")
+
+# Categories distribution ------------------------------------
+print("The plot will show the distribution of categories/labels in traning set")
+print("The distribution of training labels can be used to compare with 
+       Distribution of a testing set labels. They must have the similar nature
+       If sets were splitted equally")
+
+frequency_labels <- table(Labels_train_numbers)
+
+# Increase margin so the text is visiable and plot the bar
+par(mar=c(15,4,2,2))
+barplot(height=frequency_labels, names=ListCategories, las=2)
+
+# Save labels ------------------------------------------------
+print("Labels saving...")
+cat(sprintf("Time: %s\n", Sys.time()))
+
+write.csv(Labels_train_numbers, file="data/output/TrainLabels.csv", 
+          row.names=FALSE)
+
+cat(sprintf("Time: %s\n", Sys.time()))
+print("Labels are saved")
