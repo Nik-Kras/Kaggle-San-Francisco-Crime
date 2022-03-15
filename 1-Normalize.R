78588c631078e92f787c5d866bd9541e559838d0
Nikita <nikita.kras.kyiv@gmail.com>
2022-03-15 21:21:40 +0000

    Wrote general normalisation function that suits all numerical columns

--- 1-Normalize.R
+++ 1-Normalize.R
@@ -13,3 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Normalization -----------------------------------------------
+print("Data normalization...")
+cat(sprintf("Time: %s\n", Sys.time()))
+
+for (i in c(1:8))
+{
+  train[,i] <- rescale(as.numeric(array(unlist(train[,..i]))))
+  test[,i]  <- rescale(as.numeric(array(unlist(test[,..i]))))
+}
+
+cat(sprintf("Time: %s\n", Sys.time()))
+print("Data normalization has been finished")
+print(head(train))
