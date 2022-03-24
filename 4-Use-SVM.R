2a01e23254212e9968f89cd72c3d93fef8316465
Nikita <nikita.kras.kyiv@gmail.com>
2022-03-24 21:58:51 +0000

    SVM hyperparameters research. Validated 400 SVM models, saved their accuracies in csv table, built a 3D surfaces of accuracy over gamma and cost. Research took 19 hours. According to results Best model is pol with low parameters

--- 4-Use-SVM.R
+++ 4-Use-SVM.R
@@ -20,7 +20,8 @@ library(ggplot2)
 library(readxl)
 
 # Reduce dataset
-step <- 200
+print("Dataset reduces to accelerate the search of hyperparameters")
+step <- 50
 train_step  <- train[seq(1, nrow(train), step),]
 labels_step <- Labels_train_numbers[seq(1, nrow(train), step)]
 
@@ -33,7 +34,8 @@ dt           <- sort(sample(nrow(train_step_m), nrow(train_step_m)*0.7))
 validate_set <- train_step_m[-dt,]
 train_set    <- train_step_m[dt,]
 
-# Find best model (run only once! takes a long)
+# Find best model with Cross-Validation (run only once! takes a long)
+# print("Use of tuning, Cross-Validation - to find the best parameters")
 # tune <- tune.svm(labels_step ~ ., 
 #                  data  = train_set, 
 #                  gamma = seq(0.1, 0.2, by = .1), 
@@ -43,6 +45,99 @@ train_set    <- train_step_m[dt,]
 # print(tune$performances)
 # cat("Best parameters:\n")
 # print(tune$best.parameters)
+
+# !!! TAKES 19 HOURS !!! Run Once Only !!!
+# Find the best model comparing testing
+# I used different basises to make equal numbers in the list
+print("Search for the best parameters:")
+
+normal_gamma_list <- 3 ^(1:10) / 10000   # Range 0.0001 -- 10
+normal_cost_list  <- 2^(1:10) / 10       # Range 0.1    -- 100
+normal_kernel_list <- c("linear", "polynomial", "radial", "sigmoid")
+
+gamma_list <- rep(normal_gamma_list, 1, each=length(normal_cost_list))
+cost_list  <- rep(normal_cost_list, length(normal_gamma_list))
+
+kernel_list <- rep(normal_kernel_list, each=length(cost_list)) 
+
+gamma_list <- rep(gamma_list, length(normal_kernel_list))
+cost_list  <- rep(cost_list, length(normal_kernel_list))
+Accuracy    <- data.frame(gamma_list, cost_list, 
+                          kernel_list, accuracy=rep(0,400))
+
+cnt <- 1
+for (k in normal_kernel_list)
+{
+  for (c in normal_cost_list)
+  {
+    for (g in normal_gamma_list)
+    {
+      svm_model <- svm(labels_step ~ ., 
+                       data   = train_set,
+                       kernel = k,
+                       gamma  = g, 
+                       cost   = c)
+      svmpredict <- round( predict( svm_model, 
+                                    validate_set, 
+                                    type = "response" ))
+      t <- table( pred = svmpredict, true=validate_set$labels_step )
+      Accuracy$accuracy[cnt] <- sum(diag(t)/(sum(t)))
+      
+      
+      cat("---------------------------------------------------\n")
+      cat(sprintf("The SVM model parameters: gamma=%f, cost=%f, kernel=%s\n", 
+                  g, c, k))
+      cat(sprintf("Number of iteration: %d/400\n", cnt))
+      cat(sprintf("The accuracy is %.2f%% \n", Accuracy$accuracy[cnt]*100))
+      cat(sprintf("Time: %s\n", Sys.time()))
+      
+      cnt <- cnt + 1
+    }
+  }
+}
+write.csv(Accuracy, file="data/output/SVM/Accuracy_gamma_cost_kernel.csv", 
+          row.names=FALSE)
+
+# Make 3D plots of accuracy
+
+axx <- list(
+  title = "Cost"
+)
+
+axy <- list(
+  title = "Gamma"
+)
+
+axz <- list(
+  title = "Accuracy"
+)
+x <- normal_cost_list
+y <- normal_gamma_list
+z_lin <- matrix(Accuracy$accuracy[1:100],   nrow = 10, byrow = TRUE)
+z_pol <- matrix(Accuracy$accuracy[101:200], nrow = 10, byrow = TRUE)
+z_rad <- matrix(Accuracy$accuracy[201:300], nrow = 10, byrow = TRUE)
+z_sig <- matrix(Accuracy$accuracy[301:400], nrow = 10, byrow = TRUE)
+
+fig <- plot_ly(z=z_lin, type = 'surface') 
+fig <- fig %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz), 
+                      title = "SVM Linear")
+fig
+
+fig <- plot_ly(z=z_pol, type = 'surface') 
+fig <- fig %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz), 
+                      title = "SVM Polynomial")
+fig
+
+fig <- plot_ly(z=z_rad, type = 'surface') 
+fig <- fig %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz), 
+                      title = "SVM Radial")
+fig
+
+fig <- plot_ly(z=z_sig, type = 'surface') 
+fig <- fig %>% layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz), 
+                      title = "SVM Sigmoid")
+fig
+
 
 # Make a model
 gamma_best  <- 0.1 # tune$best.parameters$gamma # 0.1
