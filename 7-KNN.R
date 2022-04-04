510fc74c3020c327f0dc6dd8d4ad44aa8447502a
Nikita <nikita.kras.kyiv@gmail.com>
2022-04-04 22:01:07 +0100

    KNN isn't working for K>100

--- 7-KNN.R
+++ 7-KNN.R
@@ -18,7 +18,9 @@
 library(ggplot2)
 library(lattice)
 library(caret)
+library(scales)
 library(class)
+library(data.table)
 
 # Load data --------------------------------------------------
 rm(list=ls())
@@ -35,6 +37,11 @@ SubmitTable <- data.table(read.csv("./data/dataset/sampleSubmission.csv",
 # First name is "ID" all next are categories
 ListCategories <- colnames(SubmitTable)[-1]  
 
+colnames(train_labels) <- "Category"
+train_labels <- sapply(train_labels, FUN=factor)
+train_labels <- factor(train_labels)
+train <- cbind(train, Category = train_labels)
+
 # Definition of metrics ----------------------------------------
 
 # *The function which calcs accuracy or loss*
@@ -46,7 +53,7 @@ ListCategories <- colnames(SubmitTable)[-1]
 # Relatively small part of dataset to spend hours, not days
 # 878,000 samples ---> 17,560 samples
 
-step <- 50
+step <- 100
 train_step  <- train[seq(1, nrow(train), step),]
 labels_step <- train_labels[seq(1, nrow(train), step)]
 
@@ -54,10 +61,6 @@ labels_step <- train_labels[seq(1, nrow(train), step)]
 # table - doesn't include zero frequency
 frequency_labels_step <- table(labels_step)
 plot(frequency_labels_step)
-
-# Increase margin so the text is visiable and plot the bar
-par(mar=c(15,4,2,2))
-barplot(height=frequency_labels_step, names=ListCategories, las=2)
 
 cat("To find a best hyperparameters for a ML methods there is no need
      to work on whole dataset and spend huge amount of time. THat is why
@@ -79,121 +82,152 @@ train_set    <- train_step[dt,]
 labels_valid <- labels_step[-dt]
 labels_train <- labels_step[dt]
 
+# Bad piece of code DELETE
+# Prevents from errors with bug step!
+for (i in 1:39)
+{
+  train_set    <- rbind(train_set,    train[train$Category == i][1])
+  validate_set <- rbind(validate_set, train[train$Category == i][1])
+  
+  labels_train<-append(labels_train, i)
+  labels_valid<-append(labels_valid, i)
+}
+validate_set <- validate_set[,1:12]
+train_set    <- train_set[,1:12]
+
 cat("The training and validation sets were created using random indexes
      The plots of their label frequency proofs that they are not distored\n")
 
 rm(dt, step, labels_step, train_step)
 
-frequency_labels_step <- tabulate(labels_valid)
-par(mar=c(15,4,2,2))
-barplot(height=frequency_labels_step, names=ListCategories, las=2)
-
-frequency_labels_step <- tabulate(labels_train)
-par(mar=c(15,4,2,2))
-barplot(height=frequency_labels_step, names=ListCategories, las=2)
-
 # The search for the best model  -------------------------------
 
-K_check = c(100, 500, 900, 2000, 4000)
+K_check = c(1, 10, 50, 100, 501, 1000, 2000, 4000)
 Accuracy <- c()
 
-cat(sprintf("The loop is setting which will try all K from 1 to %d\n", MaxK))
+cat(sprintf("The loop is setting which will try all K from the list\n"))
 
-for (i in 1:K_check)
+for (i in K_check)
 {
   K <- i
-  knn.pred <- knn(train=train_set, test=validate_set, cl=labels_train, k=K)
-  knn.pred <- factor(as.character(knn.pred), levels = 1:39)
+  knn.pred <- knn(train=train_set, 
+                  test=validate_set, 
+                  cl=labels_train, 
+                  k=K,
+                  l=1,
+                  use.all = FALSE)
   
+  knn.pred <- factor(as.character(knn.pred), levels = 1:39)
+
   labels_valid_factor <- factor(as.character(labels_valid),
                                 levels = 1:39)
-  
+
   CM <- confusionMatrix(table(knn.pred,labels_valid_factor))
   Accuracy <- append(Accuracy, CM$overall["Accuracy"])
-  
+
   cat("---------------------------------------------------\n")
   cat(sprintf("The KNN model parameters: K=%d\n", K))
-  cat(sprintf("The accuracy is %.2f%% \n", Accuracy[i]*100))
+  cat(sprintf("The accuracy is %.2f%% \n", CM$overall["Accuracy"]*100))
   cat(sprintf("Time: %s\n", Sys.time()))
 }
 
-cat("The loop is finished. The accuracy for each K number 
+cat("The loop is finished. The accuracy for each K number
     can be seen on the plot\n")
 
-par(mar=c(5,4,2,2))
-plot(Accuracy, type="p", main = "KNN accuracy over param. K")
+#############################
+# set.seed(400)
+# knnGrid <-  expand.grid(k = c(1, 10, 50, 100, 500, 1000, 2000, 4000))
+# fitControl <- trainControl(method = "cv",
+#                            number = 10)
+# 
+# knnFit <- train(Category ~ .,
+#                 data = train_set,
+#                 method = "knn",
+#                 trControl = fitControl,
+#                 tuneGrid = knnGrid)
+# 
+# #Output of kNN fit
+# knnFit
+# 
+# knnPredict <- predict(knnFit,newdata = validate_set[,1:12])
+# confusionMatrix(knnPredict, validate_set[,13])
 
-cat(sprintf("Max accuracy has been achieved with K=%d\n", which.max(Accuracy)))
-cat("While looking at the plot it could be seen that after apx.K=30
-     The accuracy almost stops rising\n")
+# par(mar=c(5,4,2,2))
+# plot(Accuracy, type="p", main = "KNN accuracy over param. K")
+# 
+# cat(sprintf("Max accuracy has been achieved with K=%d\n", which.max(Accuracy)))
+# cat("While looking at the plot it could be seen that after apx.K=30
+#      The accuracy almost stops rising\n")
+# 
 
-# Validating the best KNN model  -------------------------------
 
-cat(sprintf("The original train dataset will be divided to 
-    Validation and Training sets. The two models will be applied for them
-    One with K=30 and another with K=%d. The model with best accuracy will
-    be used for Testing set.\n", which.max(Accuracy)))
-
-dt           <- sort(sample(nrow(train), nrow(train)*0.7))
-validate_set <- train[-dt,]
-train_set    <- train[dt,]
-labels_valid <- Labels_train_numbers[-dt]
-labels_train <- Labels_train_numbers[dt]
-
-# According to theory generall K is choosen as sqrt(nrow(train)) -> 940
-K_chosen = K_check[which.max(Accuracy)] #c(30, which.max(Accuracy))
-Accuracy_all <- c()
-
-for (K in K_chosen)
-{
-  knn.pred <- knn(train=train_set, test=validate_set, cl=labels_train, k=K)
-  knn.pred <- factor(as.character(knn.pred), levels = 1:39)
-  
-  labels_valid_factor <- factor(as.character(labels_valid),
-                                levels = 1:39)
-  
-  CM <- confusionMatrix(table(knn.pred,labels_valid_factor))
-  Accuracy_all <- append(Accuracy_all, CM$overall["Accuracy"])
-}
-
-cat(sprintf("Time: %s\n", Sys.time()))
-cat(sprintf("The accuracy is %.2f%% \n", Accuracy_all*100))
-
-par(mar=c(5,4,2,2))
-plot(Accuracy_all, y=30,
-     xlab = "K",
-     ylab = "Accuracy",
-     main = "KNN accuracy for whole dataset")
-
-write.csv(Accuracy_all, file="data/output/KNN_accuracy_all_dataset.csv", 
-          row.names=FALSE)
-
-write.csv(Accuracy, file="data/output/KNN_accuracy_each_50.csv", 
-          row.names=FALSE)
-
-# Using model for Kaggle submission  ---------------------------
-
-cat("Applying the same hyperparameters to make a predictions
-     On real testing data for a Kaggle submission\n")
-cat(sprintf("Time: %s\n", Sys.time()))
-
-knn.pred <- knn(train=train, 
-                test=test, 
-                cl=Labels_train_numbers, 
-                k=30)
-
-write.csv(knn.pred, file="data/output/Submit/KNN_output_labels.csv", 
-          row.names=FALSE)
-
-cat(sprintf("Time: %s\n", Sys.time()))
-
-# Make and save the submission  --------------------------------
-
-cat("Making submission file!")
-
-source('make_submission.R')
-
-make_submit(labesl_predict = knn.pred,
-            category_names = ListCategories,
-            name = "KNN_submission.csv",
-            path = "data/output/Submit/")
+# # Validating the best KNN model  -------------------------------
+# 
+# cat(sprintf("The original train dataset will be divided to 
+#     Validation and Training sets. The two models will be applied for them
+#     One with K=30 and another with K=%d. The model with best accuracy will
+#     be used for Testing set.\n", which.max(Accuracy)))
+# 
+# dt           <- sort(sample(nrow(train), nrow(train)*0.7))
+# validate_set <- train[-dt,]
+# train_set    <- train[dt,]
+# labels_valid <- Labels_train_numbers[-dt]
+# labels_train <- Labels_train_numbers[dt]
+# 
+# # According to theory generall K is choosen as sqrt(nrow(train)) -> 940
+# K_chosen = K_check[which.max(Accuracy)] #c(30, which.max(Accuracy))
+# Accuracy_all <- c()
+# 
+# for (K in K_chosen)
+# {
+#   knn.pred <- knn(train=train_set, test=validate_set, cl=labels_train, k=K)
+#   knn.pred <- factor(as.character(knn.pred), levels = 1:39)
+#   
+#   labels_valid_factor <- factor(as.character(labels_valid),
+#                                 levels = 1:39)
+#   
+#   CM <- confusionMatrix(table(knn.pred,labels_valid_factor))
+#   Accuracy_all <- append(Accuracy_all, CM$overall["Accuracy"])
+# }
+# 
+# cat(sprintf("Time: %s\n", Sys.time()))
+# cat(sprintf("The accuracy is %.2f%% \n", Accuracy_all*100))
+# 
+# par(mar=c(5,4,2,2))
+# plot(Accuracy_all, y=30,
+#      xlab = "K",
+#      ylab = "Accuracy",
+#      main = "KNN accuracy for whole dataset")
+# 
+# write.csv(Accuracy_all, file="data/output/KNN_accuracy_all_dataset.csv", 
+#           row.names=FALSE)
+# 
+# write.csv(Accuracy, file="data/output/KNN_accuracy_each_50.csv", 
+#           row.names=FALSE)
+# 
+# # Using model for Kaggle submission  ---------------------------
+# 
+# cat("Applying the same hyperparameters to make a predictions
+#      On real testing data for a Kaggle submission\n")
+# cat(sprintf("Time: %s\n", Sys.time()))
+# 
+# knn.pred <- knn(train=train, 
+#                 test=test, 
+#                 cl=Labels_train_numbers, 
+#                 k=30)
+# 
+# write.csv(knn.pred, file="data/output/Submit/KNN_output_labels.csv", 
+#           row.names=FALSE)
+# 
+# cat(sprintf("Time: %s\n", Sys.time()))
+# 
+# # Make and save the submission  --------------------------------
+# 
+# cat("Making submission file!")
+# 
+# source('make_submission.R')
+# 
+# make_submit(labesl_predict = knn.pred,
+#             category_names = ListCategories,
+#             name = "KNN_submission.csv",
+#             path = "data/output/Submit/")
